74b876ed2612c055884caaeb36bb6f7cd29fe921
henac <man25816@naver.com>
2023-10-16 13:07:41 +0900

    수업 전

---
+++ 10-16/src/day9/Car.java
@@ -0,0 +1,14 @@
+package day9;
+
+public class Car {
+
+	private int gasolineGauge;
+	
+	public Car(int gas) {
+		this.gasolineGauge = gas;
+	}
+	
+	public void showCurrentGauge() {
+		System.out.println("잔여 가솔린: " + this.gasolineGauge);
+	}
+}
---
+++ 10-16/src/day9/Constructor.java
@@ -0,0 +1,21 @@
+package day9;
+
+public class Constructor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		HybridWaterCar hwCar1 = new HybridWaterCar(4.2, 2);
+		hwCar1.showCurrentGauge();
+
+		HybridWaterCar hwCar2 = new HybridWaterCar(9, 5.1, 7);
+		hwCar2.showCurrentGauge();
+
+	    HybridCar hwCar3 = new HybridCar(5, 5.1);
+	    hwCar3.showCurrentGauge();
+	    
+	    Car hwCar4 = new Car(10);
+	    hwCar4.showCurrentGauge();
+	}
+
+}
