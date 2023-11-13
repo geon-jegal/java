7636d69e952e793d39475e9d9cad09de1724eb55
제갈건 <@172.16.0.36>
2023-11-13 12:52:56 +0900

    노트북 초기화 후 새로 커밋

--- 11-08/src/day15/stringGame.java
+++ 11-08/src/day15/stringGame.java
@@ -1,39 +1,10 @@
 package day15;
 
-import java.util.Random;
-import java.util.Scanner;
-
 public class stringGame {
-	
-	static String[] fruits = { "strawberry", "watermelon", "apple", "orange", "banana", "blueberry" };
-	static StringBuffer solution;
-	static String c;
 
 	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		// TODO Auto-generated method stub	
-		
-		int index = (new Random()).nextInt(fruits.length);
-
-		solution = new StringBuffer(fruits[index]);
-
-		StringBuffer answ = new StringBuffer(solution.length());
-		for(int i = 0; i < solution.length(); i++) {
-			answ.append("_");
-		}
-		
-		while(!answ.toString().equals(solution.toString())) {
-			System.out.println("Game : " + answ);
-			System.out.print("Input a character : ");
-			c = scan.next();
-			for(int i = 0; i < solution.length(); i++) {
-				if(c.charAt(0) == solution.charAt(i))  answ.replace(i, i+1, c);
-			}
-		}
-		
-		System.out.println("Corret : " + solution + "\nSuccess!!");
-		
-		scan.close();
+		fruit f = new fruit();
+		f.game();
 	}
 
 }
