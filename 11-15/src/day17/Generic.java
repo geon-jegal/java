02794f0ac9d06421d5512d3c2ab126304fc7266d
man25816 <man25816@naver.com>
2023-11-20 17:40:32 +0900

    .

--- 11-15/src/day17/Generic.java
+++ 11-15/src/day17/Generic.java
@@ -1,14 +1,6 @@
 package day17;
 
 public class Generic {
-
-	// 배열의 요소를 출력하는 제네릭 메서드
-    public static <E> void printArray(E[] array) {
-        for (E element : array) {
-            System.out.print(element + " ");
-        }
-        System.out.println();
-    }
 	
     public static <T extends Number> void swapbox(Box<T> a, Box<T> b) {
 		Box<T> tmp = new Box<>();
@@ -24,28 +16,35 @@ public class Generic {
 	    b.set(tmp.get());
 	}
     
-    
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Box<Integer> box1 = new Box<>();
-		box1.set(99);
-		
 		Box<Integer> box2 = new Box<>();
-		box2.set(55);
+		Box<Fruit> box3 = new Box<>();
+		Box<Fruit> box4 = new Box<>();
+		Box<String> box5 = new Box<>();
+		Box<String> box6 = new Box<>();
 		
+		box1.set(99);
+		box2.set(55);
+		box3.set(new Apple());
+		box4.set(new Orange());
+		box5.set("Hello");
+		box6.set("World");
+		
+		//Integer
 		System.out.println(box1.get() + " & " + box2.get());
 		swapbox(box1, box2);
-		//swapbox2(box1, box2);
 		System.out.println(box1.get() + " & " + box2.get());
 		
-		Box<Fruit> box3 = new Box<>();
-		box3.set(new Apple());
-		Box<Fruit> box4 = new Box<>();
-		box4.set(new Orange());
-		
+		//Class
 		System.out.println(box3.get() + " & " + box4.get());
 		swapbox2(box3, box4);
 		System.out.println(box3.get() + " & " + box4.get());
 		
+		//String
+		System.out.println(box5.get() + " & " + box6.get());
+		swapbox2(box5, box6);
+		System.out.println(box5.get() + " & " + box6.get());
 	}
 }
